595d246b7e5c09cc2c190863764c7a38383cba6a
mejaybird <149760812+mejaybird@users.noreply.github.com>
2024-05-23 18:39:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,12 @@ cnx = st.connection("snowflake")
 
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
 
+pd_df=my_datafame.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 
 
