3d6811d8b6dd14d9897906c55b9284f9e7ad5032
mejaybird <149760812+mejaybird@users.noreply.github.com>
2024-05-23 18:44:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,10 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
-pd_df=my_datafame.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+# st.dataframe(pd_df)
+# st.stop()
 
 
 
@@ -45,6 +46,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        
         st.subheader(fruit_chosen + 'Nutrition Informantion')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
